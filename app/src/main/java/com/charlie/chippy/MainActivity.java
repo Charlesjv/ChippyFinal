853f7878d6714f7d9d38ff400705d6248cde28c1
Charlesjv <charles764jaison@gmail.com>
2019-10-07 19:16:09 -0400

    Creating a game engine object

--- app/src/main/java/com/charlie/chippy/MainActivity.java
+++ app/src/main/java/com/charlie/chippy/MainActivity.java
@@ -2,15 +2,43 @@ package com.charlie.chippy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
 
 //Main Activity
 public class MainActivity extends AppCompatActivity {
+    GameEngine chippy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Get size of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        // Initialize the GameEngine object
+        // Pass it the screen size (height & width)
+       chippy = new GameEngine(this, size.x, size.y);
+
+        // Make GameEngine the view of the Activity
+        setContentView(chippy);
+    }
+
+    // Android Lifecycle function
+    @Override
+    protected void onResume() {
+        super.onResume();
+        chippy.startGame();
+    }
+
+    // Stop the thread in snakeEngine
+    @Override
+    protected void onPause() {
+        super.onPause();
+        chippy.pauseGame();
     }
 }
